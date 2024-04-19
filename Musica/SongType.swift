040691aefa15765a2ccaf89c4e85ef4f203ce321
Ricardo Olvera <140466264+RicardoOlveraVe@users.noreply.github.com>
2024-04-19 03:19:51 -0600

    actualizacion

--- Musica/SongType.swift
+++ Musica/SongType.swift
@@ -7,6 +7,9 @@ enum SongType {
         switch self {
         case .song: return [
             CaptionedMusic(image: .ellaSeFue, name: "Ella se fue", artist: "KintoSol", song: "ellaSeFue", songType: .ellaSeFue),
+            CaptionedMusic(image: .ellaSeFue, name: "Ella se fue", artist: "KintoSol", song: "ellaSeFue", songType: .ellaSeFue),
+            CaptionedMusic(image: .ellaSeFue, name: "Ella se fue", artist: "KintoSol", song: "ellaSeFue", songType: .ellaSeFue),
+            CaptionedMusic(image: .ellaSeFue, name: "Ella se fue", artist: "KintoSol", song: "ellaSeFue", songType: .ellaSeFue),
             
             CaptionedMusic(image: .morire, name: "Moriré", artist: "La Factoria", song: "morire", songType: .morire)
         ]
